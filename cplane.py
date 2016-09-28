6d4a26d8c5ad0c20fc408cd9cd9e81f765face1a
Lance Clifner <clifn100@mail.chapman.edu>
2016-09-28 19:04:26 +0000

    fix syntax problems so that the creator will work and can start making test cases. cut down on default dimensions of the creator's plane so it is easier to generate the expected data cases for the test cases.

--- cplane.py
+++ cplane.py
@@ -4,24 +4,27 @@ import abscplane as cp
 
 class ComplexPlane(cp.AbsComplexPlane):
     
+
     def __init__(self):
-        self.xmin  = -100.
-        self.xmax  = 100.
-        self.xlen  = 200. #make sure this is never able to be 0
-        self.ymin  = -100.
-        self.ymax  = 100.
-        self.ylen  = 200. #make sure this can never be 0
+        self.xmin  = -5.
+        self.xmax  = 5.
+        self.xlen  = 10  #make sure this is never able to be 0
+        self.ymin  = -5.
+        self.ymax  = 5.
+        self.ylen  = 10  #make sure this can never be 0
         self.xstep = (self.xmax - self.xmin)/self.xlen
         self.ystep = (self.ymax - self.ymin)/self.ylen
-        self.f     = lambda x: 2*x
-        self.plane = [[(( j*xstep + xmin ) + ( i*ystep + ymin )*1j) for i in range(ylen)] for j in range(xmin,xmax)]
+        self.f     = lambda x: x
+        self.plane = [[self.f(( j*self.xstep + self.xmin ) + ( i*self.ystep + self.ymin )*1j) for i in range(self.ylen)] for j in range(self.xlen)]
+
 
     def refresh(self):
         """Regenerate complex plane.
         For every point (x + y*1j) in self.plane, replace
         the point with the value self.f(x + y*1j). 
         """
-        self.plane = [[self.f(( j*xstep + xmin ) + ( i*ystep + ymin )*1j) for i in range(ylen)] for j in range(xmin,xmax)]
+        self.plane = [[self.f(( j*self.xstep + self.xmin ) + ( i*self.ystep + self.ymin )*1j) for i in range(self.ylen)] for j in range(self.xlen)]
+
 
     def zoom(self):
         """Reset self.xmin, self.xmax, and/or self.xlen.
